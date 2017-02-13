60cba155d7acfae4f418a598d7e7336e82483706
dongmy54 <615760433@qq.com>
2017-02-13 18:33:02 +0800

    implement add_product_to_cart function

---
+++ app/models/cart_item.rb
@@ -0,0 +1,5 @@
+class CartItem < ApplicationRecord
+	belongs_to :Cart
+	belongs_to :product
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ end
 
 resources :products do
 	member do
-		post: add_to_cart
+		post :add_to_cart
 		end
 	end
 
