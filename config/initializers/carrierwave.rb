694d7918ad8057bc63c4c99f4a13a97b4bad8371
dongmy54 <615760433@qq.com>
2017-02-17 20:22:52 +0800

    fix aws key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.production? 
-    config.storage :fog                       
+    config.fog_provider = 'fog'                  
     config.fog_credentials = {
       provider:              'AWS',                        
-      aws_access_key_id:     "AKIAJD5U46EB5KCZYIVA" ,         
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],         
 
-      aws_secret_access_key: "RkFq0zfwZioOpfOtKEEMydDYRjCowaIGtTrIIMJv" ,        
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],        
 
-      region:                'us-west-2' # 你的S3 bucket 的 Region 位置    
+      region:                ENV["AWS_REGION"]    
 
     }
     config.fog_directory  = ENV["AWS_BUCKET_NAME"] 
@@ -19,6 +19,7 @@ CarrierWave.configure do |config|
   else
     config.storage :file
   end
+end
 
 
 
