6b1bee38843e4d5a5fefd07bce62402daa0600e6
dongmy54 <615760433@qq.com>
2017-02-17 17:45:04 +0800

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,9 +6,9 @@ CarrierWave.configure do |config|
     config.storage :fog                       
     config.fog_credentials = {
       provider:              'AWS',                        
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],         
+      aws_access_key_id:     "AKIAJD5U46EB5KCZYIVA" ,         
 
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],        
+      aws_secret_access_key: "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" ,        
 
       region:                'us-west-2' # 你的S3 bucket 的 Region 位置    
 
