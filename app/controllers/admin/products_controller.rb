1b0491a64e56b7eb49efc5fb3877cd364bc61d49
dongmy54 <615760433@qq.com>
2017-02-21 19:48:07 +0800

    change controller admin/product and product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -11,16 +11,28 @@ class Admin::ProductsController < ApplicationController
 
   def new
   	@product = Product.new
+    @photo = @product.photos.build
   end
 
   def create
   	@product = Product.new(product_params)
 
   	if @product.save
-  		redirect_to admin_products_path
-  	else
+  		if params[:photos] != nil
+        params[:photos]['image'].each do |a|
+          @photo = @product.photos.create(:image => a)
+        end
+      end
+            redirect_to admin_products_path 
+
+    else
   		render :new
   	end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    @photos = @product.photos.all
   end
 
   def edit
@@ -30,8 +42,16 @@ class Admin::ProductsController < ApplicationController
   def update
   	@product = Product.find(params[:id])
 
-  	if @product.update(product_params)
-  		redirect_to products_path 
+  	if params[:photos] != nil
+      @product.photos.destroy_all
+      params[:photos]['image'].each do |a|
+        @picture = @product.photos.create(:image => a)
+      end
+      @product.update(product_params)
+      redirect_to admin_products_path
+
+    elsif @product.update(product_params)
+      redirect_to admin_products_path
   	else
   		render :edit
   	end
