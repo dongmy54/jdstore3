2fa9eb67b0f49c547017decc362f913e712e931e
dongmy54 <615760433@qq.com>
2017-03-07 12:17:30 +0800

    test

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
-
+skip_before_filter :verify_authenticity_token
+protect_from_forgery prepend: true, with: :exception
   # GET /resource/sign_up
   # def new
   #   super
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
-
+skip_before_filter :verify_authenticity_token
+protect_from_forgery prepend: true, with: :exception
   # GET /resource/sign_in
   # def new
   #   super
