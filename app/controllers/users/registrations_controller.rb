d9908267275b806524b3d7fae9e92f50fc429d4a
dongmy54 <615760433@qq.com>
2017-03-07 12:29:33 +0800

    test

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
-skip_before_filter :verify_authenticity_token
-protect_from_forgery prepend: true, with: :exception
-before_action :authenticate_user!
+
   # GET /resource/sign_up
   # def new
   #   super
