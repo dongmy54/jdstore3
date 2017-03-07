053bddfe21a4954dfde2548ed6398de7494bebd8
dongmy54 <615760433@qq.com>
2017-03-07 09:14:05 +0800

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
 
   def admin_required
   	if !current_user.admin?
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
+  skip_before_filter :verify_authenticity_token, :only => :create
 
 # before_action :configure_sign_in_params, only: [:create]
 
