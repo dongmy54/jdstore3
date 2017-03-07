267d1bed3b0ebf1c2c81e8f4ebb288e5c6214c5a
dongmy54 <615760433@qq.com>
2017-03-07 22:29:16 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'https://stormy-savannah-55425.herokuapp.com/'}
+  config.action_mailer.default_url_options = { :host => 'https://ilife-.herokuapp.com'}
 
    config.action_mailer.delivery_method = :smtp
    ActionMailer::Base.smtp_settings = {
