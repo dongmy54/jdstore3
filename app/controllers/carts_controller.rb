7c0a91a1ab3e600b9c7372be43707957a82c9bce
dongmy54 <615760433@qq.com>
2017-09-18 15:27:27 +0800

    fix a bug

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,10 @@
 class CartsController < ApplicationController
 
-
+def index
+  @cart_items = current_cart.cart_items.includes(:product => :photos)
+end
 
 def clean
-	
 	current_cart.clean!
 	redirect_to carts_path
 	flash[:warning] = "已清空购物车"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
+
 	def collections_count
 		if current_user.blank?
 			return 0
 			else
-				return current_user.collections.count
+				return current_user.collections_count
 		end
 	end
+
 end
